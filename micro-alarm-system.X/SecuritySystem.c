b1ae4d3b2c227e4146e85de804e3d79913e29fd9
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2021-12-28 17:39:28 +0100

    Added new wrapper methods

--- micro-alarm-system.X/SecuritySystem.c
+++ micro-alarm-system.X/SecuritySystem.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <p32xxxx.h>
+#include "Speaker.h"
 
 int securitySystemEnabled = 0;
 
@@ -11,11 +12,22 @@ int SECURITY_isSecuritySystemEnabled () {
     return securitySystemEnabled;
 }
 
+void SECURITY_startAlarm() {
+    SPEAKER_Start();
+}
+
+void SECURITY_stopAlarm() {
+    SPEAKER_Stop();
+}
+
 /**
  * Enable security system
  */
 void SECURITY_enable() {
     securitySystemEnabled = 1;
+    
+    // FIXME: start buzzer only when board moved
+    SECURITY_startAlarm();
 }
 
 /**
@@ -23,4 +35,7 @@ void SECURITY_enable() {
  */
 void SECURITY_disable() {
     securitySystemEnabled = 0;
+    
+    // stop buzzing
+    SECURITY_stopAlarm();
 }
--- micro-alarm-system.X/SecuritySystem.h
+++ micro-alarm-system.X/SecuritySystem.h
@@ -1,3 +1,5 @@
 int SECURITY_isSecuritySystemEnabled ();
 void SECURITY_enable();
 void SECURITY_disable();
+void SECURITY_startAlarm();
+void SECURITY_stopAlarm();
